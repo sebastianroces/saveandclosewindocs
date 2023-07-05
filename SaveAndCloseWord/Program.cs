0d080d5ddf5206be1bff264193ef8ebd8c3abf24
sebastianroces <137846123+sebastianroces@users.noreply.github.com>
2023-07-05 16:52:52 -0400

    Add files via upload

---
+++ SaveAndCloseWord/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using Win32_API;
+using System.Globalization;
+
+namespace SaveAndCloseWord
+{
+    class Program
+    {
+        [DllImport("User32.dll")]
+        private static extern bool
+         GetLastInputInfo(ref LASTINPUTINFO plii);
+
+        static void Main(string[] args)
+        {
+            Run();
+            
+        }
+
+        static void Run()
+        {
+            while (true)
+            {
+                System.Threading.Thread.Sleep(600000);
+                if (Win32.GetIdleTime() > 600000 && (System.DateTime.Now.Hour > 6 && System.DateTime.Now.Minute > 10 && System.DateTime.Now.ToString("tt", CultureInfo.InvariantCulture) == "PM"))
+                {
+                    foreach (var process in System.Diagnostics.Process.GetProcessesByName("winword"))
+                    {
+                        try
+                        {
+                            Console.Write("The Time is " + System.DateTime.Now.Hour.ToString() + ":" + System.DateTime.Now.Minute +" " + System.DateTime.Now.ToString("tt", CultureInfo.InvariantCulture));
+                            Console.WriteLine(" Time to close word");
+                            process.Kill();
+                        }
+                        catch { }
+                    }
+                }
+            }
+        }
+
+    }
+}
